6cd1fa39eb32b6ff7339a2c44e7d44fb55068dfe
Manas Sharma <manasdotsharma@gmail.com>
2024-08-05 17:23:55 +0530

    add code walkthrough and make sure quickstart is always at the top

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,13 +1,19 @@
-/** @type { import('@storybook/react').Preview } */
-
 import "../basic-tailwind-imports-for-html-files.css";
 
+/** @type { import('@storybook/react').Preview } */
 const preview = {
   tags: ["autodocs"],
   parameters: {
     options: {
-      storySort: {
-        order: [],
+      storySort: (a, b) => {
+        if (a.title === "Quickstart") {
+          return -1;
+        }
+        if (b.title === "Quickstart") {
+          return 1;
+        }
+
+        return undefined;
       },
     },
   },
