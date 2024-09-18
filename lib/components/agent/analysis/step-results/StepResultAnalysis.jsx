89bf7b68707da6ef52d73b835da705648c9a6b8a
Manas Sharma <manasdotsharma@gmail.com>
2024-09-18 17:21:46 +0530

    default to gray loader instead of white. put analyse data inside use effect. restyle the analysis section

--- lib/components/agent/analysis/step-results/StepResultAnalysis.jsx
+++ lib/components/agent/analysis/step-results/StepResultAnalysis.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import { message, Spin } from "antd";
+import { useContext, useEffect, useRef, useState } from "react";
 import setupBaseUrl from "../../../utils/setupBaseUrl";
+import { MessageManagerContext, SpinningLoader } from "@ui-components";
 
 export default function StepResultAnalysis({
   keyName,
@@ -8,60 +8,69 @@ export default function StepResultAnalysis({
   data_csv,
   apiEndpoint,
   sql,
-  image = null,
 }) {
   const [toolRunAnalysis, setToolRunAnalysis] = useState("");
   const [loading, setLoading] = useState(false);
-
-  async function analyseData() {
-    const urlToConnect = setupBaseUrl({
-      protocol: "http",
-      path: "analyse_data",
-      apiEndpoint: apiEndpoint,
-    });
-    
-    // send data to the server
-    const data = {
-      question: question,
-      data_csv: data_csv,
-      sql: sql,
-      key_name: keyName,
-    };
-
-    setLoading(true);
-    const response = await fetch(urlToConnect, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (!response.ok) {
-      message.error("Error analysing data");
-      return;
-    }
-    const responseJson = await response.json();
-    
-    console.log("Received response from server")
-    console.log(responseJson);
-    setToolRunAnalysis(responseJson.model_analysis);
-    setLoading(false);
-  }
+  const messageManager = useContext(MessageManagerContext);
 
   useEffect(() => {
+    async function analyseData() {
+      try {
+        const urlToConnect = setupBaseUrl({
+          protocol: "http",
+          path: "analyse_data",
+          apiEndpoint: apiEndpoint,
+        });
+
+        // send data to the server
+        const data = {
+          question: question,
+          data_csv: data_csv,
+          sql: sql,
+          key_name: keyName,
+        };
+
+        setLoading(true);
+        const response = await fetch(urlToConnect, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+          throw new Error("Error analysing data");
+        }
+
+        const responseJson = await response.json();
+        setToolRunAnalysis(responseJson.model_analysis);
+      } catch (error) {
+        console.error(error);
+        messageManager.error("Error analysing data");
+      } finally {
+        setLoading(false);
+      }
+    }
+
     analyseData();
   }, []);
 
   return (
-    <div style={{ whiteSpace: "pre-wrap" }} className="max-w-2xl w-full">
-      {loading === true ?
+    <div
+      style={{ whiteSpace: "pre-wrap" }}
+      className="bg-gray-100 rounded my-3 text-xs text-gray-400 p-4"
+    >
+      {loading === true ? (
         <>
-        <p className="small code p-2">Loading Analysis...</p> <Spin />
+          <p className="">
+            <SpinningLoader />
+            Loading Analysis
+          </p>
         </>
-        : <p className="small code p-2">{toolRunAnalysis}</p>
-      }
-      
+      ) : (
+        <p className="">{toolRunAnalysis}</p>
+      )}
     </div>
   );
 }
