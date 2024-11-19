adc491fa5ba984ae6c441dc966d6d9577f9ad9c2
Manas Sharma <manasdotsharma@gmail.com>
2024-11-19 11:14:23 +0530

    tweak table stories to have a blank cell example

--- test/ui-components/test-ui-components.jsx
+++ test/ui-components/test-ui-components.jsx
@@ -5,11 +5,7 @@ import { TextArea, useWindowSize } from "@ui-components";
 
 function TestWindowSizeHook() {
   const size = useWindowSize();
-  return (
-    <div>
-      <TextArea value={size.join(",")} />
-    </div>
-  );
+  return <div>Run npm run storybook to see the ui components.</div>;
 }
 ReactDOM.createRoot(document.getElementById("root")).render(
   <TestWindowSizeHook />
