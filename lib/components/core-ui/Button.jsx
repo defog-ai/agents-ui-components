7887a1e37fa6edb679f62d6d8964aeaba3d7877e
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-12-10 23:27:25 +0800

    implement dark mode for button and table

--- lib/components/core-ui/Button.jsx
+++ lib/components/core-ui/Button.jsx
@@ -3,15 +3,15 @@ import { twMerge } from "tailwind-merge";
 
 const variantStyles = {
   normal:
-    "border bg-gray-50 text-gray-800 hover:bg-gray-100 hover:text-gray-800 active:text-gray-800",
+    "border border-gray-300 dark:border-gray-600 bg-gray-50 dark:bg-gray-700 text-gray-800 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-gray-800 dark:hover:text-gray-100 active:text-gray-800 dark:active:text-gray-100",
   primary:
-    "bg-blue-500 text-white hover:bg-blue-600 hover:text-white active:text-white",
+    "bg-blue-500 text-white hover:bg-blue-600 hover:text-white active:text-white dark:bg-blue-600 dark:hover:bg-blue-700",
   danger:
-    "bg-red-500 text-white hover:bg-red-600 hover:text-white active:text-white",
+    "bg-red-500 text-white hover:bg-red-600 hover:text-white active:text-white dark:bg-red-600 dark:hover:bg-red-700",
 };
 
 const disabledStyles =
-  "bg-gray-50 text-gray-300 hover:bg-gray-50 hover:text-gray-300 cursor-not-allowed active:bg-gray-50 active:text-gray-300 active:brightness-[100%] hover:active:brightness-[100%] active:[&:not(:hover)]:brightness-[100%]";
+  "bg-gray-50 dark:bg-gray-700 text-gray-300 dark:text-gray-500 hover:bg-gray-50 dark:hover:bg-gray-700 hover:text-gray-300 dark:hover:text-gray-500 cursor-not-allowed active:bg-gray-50 dark:active:bg-gray-700 active:text-gray-300 dark:active:text-gray-500 active:brightness-[100%] hover:active:brightness-[100%] active:[&:not(:hover)]:brightness-[100%]";
 
 /**
  * @typedef {Object} ButtonProps
