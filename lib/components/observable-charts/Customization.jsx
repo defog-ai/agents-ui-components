d3dac4f44141aab91c248f746db5a03e3be2c966
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-04 01:41:38 +0800

    delete unused code

--- lib/components/observable-charts/Customization.jsx
+++ lib/components/observable-charts/Customization.jsx
@@ -1,18 +1,13 @@
-import { Slider } from "@ui-components";
 import { ColorPicker } from "antd";
 import LineChartControls from "./chart-controls/LineChartControls";
 import ScatterPlotControls from "./chart-controls/ScatterPlotControls";
 import BarChartControls from "./chart-controls/BarChartControls";
-import AxisControl from "./components/AxisControl";
-import D3DateFormatBuilder from "./components/DateFormatter";
-import ColorSchemeSelector from "./components/ColorschemeSelector";
 import { ChartManagerContext } from "./ChartManagerContext";
 import { useContext } from "react";
 
 export function Customization() {
   const chartManager = useContext(ChartManagerContext);
-  const { selectedChart, chartStyle, selectedColumns, availableColumns } =
-    chartManager.config;
+  const { selectedChart, chartStyle } = chartManager.config;
 
   const handleStyleChange = (key, value) => {
     chartManager.updateChartStyle({ [key]: value }).render();
@@ -31,10 +26,6 @@ export function Customization() {
         return null;
     }
   };
-
-  const isDateColumnSelected = availableColumns.some(
-    (col) => col.key === selectedColumns.x && col.isDate
-  );
 
   return (
     <div className="grid grid-rows-[auto_1fr] max-h-[550px] overflow-x-clip overflow-y-auto h-full">
