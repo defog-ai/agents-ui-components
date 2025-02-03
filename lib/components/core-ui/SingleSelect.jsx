5481a96152cdf92315caf2ecdc50e156eef1b629
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-04 01:36:24 +0800

    nicer hovers over select elements

--- lib/components/core-ui/SingleSelect.jsx
+++ lib/components/core-ui/SingleSelect.jsx
@@ -203,7 +203,7 @@ export function SingleSelect({
           type="text"
           placeholder={placeholder}
           className={twMerge(
-            "w-full rounded-md border-0 pr-12 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 focus:ring-2 focus:ring-inset focus:ring-blue-400 dark:focus:ring-blue-500 sm:text-sm sm:leading-6",
+            "w-full rounded-md border-0 pr-12 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-600 focus:ring-2 focus:ring-inset focus:ring-blue-400 dark:focus:ring-blue-500 sm:text-sm sm:leading-6 hover:ring-2 hover:ring-inset hover:ring-blue-400 dark:hover:ring-blue-500 hover:cursor-pointer",
             inputSizeClasses[size] || inputSizeClasses["default"],
             disabled
               ? "bg-gray-100 dark:bg-gray-800 text-gray-400 dark:text-gray-500"
