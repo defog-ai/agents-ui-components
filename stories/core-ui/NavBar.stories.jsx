fd5dfb1abe39a495aeb2f7957dac12063e587b97
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-07 01:37:56 +0800

    add a story for the navbar

---
+++ stories/core-ui/NavBar.stories.jsx
@@ -0,0 +1,63 @@
+import { NavBar } from "../../lib/components/core-ui/NavBar";
+
+export default {
+  title: "Core UI/NavBar",
+  component: NavBar,
+  decorators: [
+    () => (
+      <div>
+        <NavBar
+          items={[
+            { title: "Home", href: "/", current: true },
+            { title: "About", href: "/about", current: false },
+            {
+              title: "Services",
+              href: "#",
+              current: false,
+              children: [
+                {
+                  title: "Consulting",
+                  href: "/services/consulting",
+                  current: false,
+                },
+                {
+                  title: "Development",
+                  href: "/services/development",
+                  current: false,
+                },
+              ],
+            },
+            { title: "Contact", href: "/contact", current: false },
+          ]}
+        />
+        <div style={{ marginTop: "20px" }}>
+          <h1>Welcome to our website!</h1>
+          <p>
+            This is some additional content to demonstrate how the NavBar works
+            together with other content on the page.
+          </p>
+        </div>
+      </div>
+    ),
+  ],
+};
+
+const Template = (args) => <NavBar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  items: [
+    { title: "Home", href: "/", current: true },
+    { title: "About", href: "/about", current: false },
+    {
+      title: "Services",
+      href: "#",
+      current: false,
+      children: [
+        { title: "Consulting", href: "/services/consulting", current: false },
+        { title: "Development", href: "/services/development", current: false },
+      ],
+    },
+    { title: "Contact", href: "/contact", current: false },
+  ],
+};
