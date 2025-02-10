838e246196632def957bd77ffbea01eaa244df85
Manas Sharma <sharma.manas271196@gmail.com>
2025-02-10 15:07:29 +0530

    also export oraclereport from this to not use multiple instances of tiptap

---
+++ lib/components/oracle/OracleReport.tsx
@@ -0,0 +1,8 @@
+import { EditorProvider, EditorProviderProps } from "@tiptap/react";
+
+/**
+ * Renders an Oracle report
+ */
+export function OracleReport(props: EditorProviderProps) {
+  return <EditorProvider {...props}></EditorProvider>;
+}
