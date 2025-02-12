d0204fc7b5645b5560535f281b01adc00c67336a
Manas Sharma <sharma.manas271196@gmail.com>
2025-02-12 13:40:33 +0530

    group reports in sidebar similar to query data

--- lib/components/oracle/reports/OracleEmbed.tsx
+++ lib/components/oracle/reports/OracleEmbed.tsx
@@ -11,6 +11,7 @@ import { getApiKeyNames } from "../../utils/utils";
 import {
   fetchReports,
   OracleReport,
+  oracleReportTimestamp,
   ReportData,
   ReportListItem,
 } from "@oracle";
@@ -24,9 +25,32 @@ interface OracleReportType extends ReportListItem {
 
 interface ReportHistory {
   [apiKeyName: string]: {
-    [reportId: string]: OracleReportType;
+    Today: { [reportId: string]: OracleReportType };
+    Yesterday: { [reportId: string]: OracleReportType };
+    "Past week": { [reportId: string]: OracleReportType };
+    "Past month": { [reportId: string]: OracleReportType };
+    Earlier: { [reportId: string]: OracleReportType };
   };
 }
+
+const findReportGroupInHistory = (
+  apiKeyName: string,
+  reportId: string,
+  history: ReportHistory
+) => {
+  // default to Today
+  if (!history[apiKeyName]) return "Today";
+  const groups = Object.keys(history[apiKeyName]);
+
+  for (const group of groups) {
+    if (history[apiKeyName][group][reportId]) {
+      return group;
+    }
+  }
+
+  // default to Today
+  return "Today";
+};
 
 /**
  * Renders an Oracle report in an embedded mode.
@@ -56,10 +80,25 @@ export function OracleEmbed({ apiEndpoint }: { apiEndpoint: string }) {
 
         setSelectedApiKeyName(keyNames[0]);
 
-        const histories = {};
+        const histories: ReportHistory = {};
+
+        const today = new Date();
+        const yesterday = new Date(today);
+        yesterday.setDate(yesterday.getDate() - 1);
+        const week = new Date(today);
+        week.setDate(week.getDate() - 7);
+        const month = new Date(today);
+        month.setMonth(month.getMonth() - 1);
 
         for (const keyName of keyNames) {
-          histories[keyName] = {};
+          histories[keyName] = {
+            Today: {},
+            Yesterday: {},
+            "Past week": {},
+            "Past month": {},
+            Earlier: {},
+          };
+
           try {
             const reports = await fetchReports(
               apiEndpoint,
@@ -71,8 +110,20 @@ export function OracleEmbed({ apiEndpoint }: { apiEndpoint: string }) {
             reports
               // Filter out reports that are not done
               .filter((report) => report.status === "done")
+              // add to histories based on date created
               .forEach((report) => {
-                histories[keyName][report.report_id] = report;
+                const date = new Date(report.date_created);
+                if (date.toDateString() === today.toDateString()) {
+                  histories[keyName]["Today"][report.report_id] = report;
+                } else if (date.toDateString() === yesterday.toDateString()) {
+                  histories[keyName]["Yesterday"][report.report_id] = report;
+                } else if (date >= week) {
+                  histories[keyName]["Past week"][report.report_id] = report;
+                } else if (date >= month) {
+                  histories[keyName]["Past month"][report.report_id] = report;
+                } else {
+                  histories[keyName]["Earlier"][report.report_id] = report;
+                }
               });
           } catch (error) {
             setError("Failed to fetch reports for key name " + keyName);
@@ -119,7 +170,7 @@ export function OracleEmbed({ apiEndpoint }: { apiEndpoint: string }) {
             "w-72 p-4 rounded-tl-lg relative sm:block min-h-96 max-h-full overflow-auto"
           }
         >
-          <div className="space-y-2">
+          <div className="space-y-4">
             <SingleSelect
               label="Select Database"
               value={selectedApiKeyName}
@@ -147,23 +198,33 @@ export function OracleEmbed({ apiEndpoint }: { apiEndpoint: string }) {
                 <SquarePen /> New
               </span>
             </div>
-            {Object.values(reportHistory[selectedApiKeyName]).map(
-              (report: OracleReportType) => (
-                <div
-                  key={report.report_id}
-                  className={twMerge(
-                    "title hover:cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700 history-item p-2 text-sm",
-                    report.report_id === selectedReportId
-                      ? "font-bold bg-gray-100 dark:bg-gray-800 border-l-2 border-l-blue-500"
-                      : ""
-                  )}
-                  onClick={() => {
-                    setSelectedReportId(report.report_id);
-                  }}
-                >
-                  <span>{report.report_name}</span>
-                </div>
-              )
+            {Object.entries(reportHistory[selectedApiKeyName]).map(
+              ([group, reports]) => {
+                if (Object.keys(reports).length === 0) return null; // Skip empty groups
+                return (
+                  <div key={group} className="mb-6">
+                    <div className="px-2 mb-2 text-xs font-medium tracking-wide text-blue-400 uppercase">
+                      {group}
+                    </div>
+                    {Object.values(reports).map((report: OracleReportType) => (
+                      <div
+                        key={report.report_id}
+                        className={twMerge(
+                          "title hover:cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700 history-item p-2 text-sm",
+                          report.report_id === selectedReportId
+                            ? "font-bold bg-gray-100 dark:bg-gray-800 border-l-2 border-l-blue-500"
+                            : ""
+                        )}
+                        onClick={() => {
+                          setSelectedReportId(report.report_id);
+                        }}
+                      >
+                        <span>{report.report_name}</span>
+                      </div>
+                    ))}
+                  </div>
+                );
+              }
             )}
           </div>
         </Sidebar>
@@ -176,13 +237,23 @@ export function OracleEmbed({ apiEndpoint }: { apiEndpoint: string }) {
               keyName={selectedApiKeyName}
               token={token.current}
               onReportParsed={(data: ReportData) => {
+                // find the group of this report in histories
+                const group = findReportGroupInHistory(
+                  selectedApiKeyName,
+                  selectedReportId,
+                  reportHistory
+                );
+
                 setReportHistory((prev) => ({
                   ...prev,
                   [selectedApiKeyName]: {
                     ...prev[selectedApiKeyName],
-                    [selectedReportId]: {
-                      ...prev[selectedApiKeyName][selectedReportId],
-                      reportData: data,
+                    [group]: {
+                      ...prev[selectedApiKeyName][group],
+                      [selectedReportId]: {
+                        ...prev[selectedApiKeyName][group][selectedReportId],
+                        reportData: data,
+                      },
                     },
                   },
                 }));
@@ -209,10 +280,14 @@ export function OracleEmbed({ apiEndpoint }: { apiEndpoint: string }) {
                       ...prev,
                       [keyName]: {
                         ...prev[keyName],
-                        [reportId]: {
-                          report_id: reportId,
-                          report_name: userQuestion,
-                          status,
+                        Today: {
+                          ...prev[keyName]["Today"],
+                          [reportId]: {
+                            report_id: reportId,
+                            report_name: userQuestion,
+                            status,
+                            date_created: oracleReportTimestamp(),
+                          },
                         },
                       },
                     }));
