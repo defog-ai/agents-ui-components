ec0045e20f4b8fefe181a16275e2264f368bde37
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-19 03:07:12 +0800

    fixed SQL Feedback

--- lib/components/agent/analysis/step-results/SQLFeedback.jsx
+++ lib/components/agent/analysis/step-results/SQLFeedback.jsx
@@ -1,68 +1,31 @@
-import React, { useState, useCallback, useContext } from "react";
-import { MessageManager, Modal, Tabs } from "@ui-components";
+import { useState, useCallback, useContext } from "react";
+import { MessageManagerContext, Modal, Tabs } from "@ui-components";
 import CodeMirror, { EditorView } from "@uiw/react-codemirror";
 import { sql as codemirrorSql } from "@codemirror/lang-sql";
 
-const SQLFeedback = ({
-  question,
-  sql,
-  previous_context,
-  apiEndpoint,
-  token,
-  keyName,
-  analysisId,
-}) => {
+const SQLFeedback = ({ question, sql, apiEndpoint, token, keyName }) => {
   const [modalVisible, setModalVisible] = useState(false);
   const [loading, setLoading] = useState(false);
   const [userProvidedSql, setUserProvidedSql] = useState("\n\n\n\n\n\n\n");
 
-  const message = useContext(MessageManager);
+  const message = useContext(MessageManagerContext);
 
   const sqlOnChange = useCallback((val, viewUpdate) => {
     setUserProvidedSql(val);
   }, []);
 
-  const uploadFeedback = async (feedbackType, feedbackText) => {
-    setLoading(true);
-    const url = apiEndpoint + "/feedback";
-    const data = {
-      previous_context: previous_context,
-      feedback: feedbackType,
-      text: feedbackText,
-      db_name: keyName,
-      token: token,
-      response: {
-        question: question,
-        generatedSql: sql,
-        questionId: analysisId,
-      },
-    };
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    if (result.status === "received" || result.status === "success") {
-      message.success("Feedback submitted successfully");
-    } else {
-      message.error("Failed to submit feedback");
-    }
-    setLoading(false);
-    setModalVisible(false);
-  };
-
   const updateGoldenQueries = async () => {
     setLoading(true);
-    const url = apiEndpoint + "/integration/update_single_golden_query";
+    const url = apiEndpoint + "/integration/update_golden_queries";
     const data = {
-      previous_context: previous_context,
-      question: question,
-      sql: userProvidedSql,
       token: token,
       db_name: keyName,
+      golden_queries: [
+        {
+          question: question,
+          sql: userProvidedSql,
+        },
+      ],
     };
     const response = await fetch(url, {
       method: "POST",
@@ -72,7 +35,7 @@ const SQLFeedback = ({
       body: JSON.stringify(data),
     });
     const result = await response.json();
-    if (result.status === "received" || result.status === "success") {
+    if (result.success === true) {
       message.success("Feedback submitted successfully");
     } else {
       message.error("Failed to submit feedback");
@@ -85,14 +48,6 @@ const SQLFeedback = ({
     <div className="h-10 mt-2">
       {/* thumbs up or thumbs down feedback */}
       <div className="flex items-left h-10 gap-1">
-        <div
-          className="flex items-center justify-center w-10 h-10 rounded-full cursor-pointer"
-          onClick={() => {
-            uploadFeedback("good", "");
-          }}
-        >
-          <span className="text-md">👍</span>
-        </div>
         <div
           className="flex items-center justify-center w-10 h-10 rounded-full cursor-pointer"
           onClick={() => {
@@ -110,55 +65,31 @@ const SQLFeedback = ({
         footer={null}
         width={768}
         onCancel={() => setModalVisible(false)}
+        rootClassNames="dark:bg-gray-800 bg-white z-50"
       >
         {/* first, a section to provide simple text feedback */}
         <Tabs
           disableSingleSelect={true}
-          defaultSelected="Give Feedback"
+          defaultSelected="Provide Correct SQL"
           tabs={[
-            // just a simple text feedback
-            {
-              name: "Give Feedback",
-              content: (
-                <div className="mt-4">
-                  <p className="mb-2">
-                    Teach the model what was wrong with the generated SQL query.
-                    This will help improve the model.
-                  </p>
-                  <textarea
-                    className="w-full h-32"
-                    placeholder="Add your feedback here"
-                    id={`${question}-feedback-text`}
-                  />
-                  <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs"
-                    onClick={() => {
-                      const feedbackText = document.getElementById(
-                        `${question}-feedback-text`
-                      ).value;
-                      uploadFeedback("bad", feedbackText);
-                    }}
-                  >
-                    Give Feedback
-                  </button>
-                </div>
-              ),
-            },
             // a section to provide a new SQL query
             {
               name: "Provide Correct SQL",
               content: (
-                <div className="mt-4">
+                <div className="mt-4 dark:text-gray-200">
                   <p className="mb-2">
-                    Optionally, please provide the correct SQL query for
-                    answering this question. As a reminder, the question was:{" "}
-                    <span className="font-mono font-semibold">{question}</span>
+                    Please provide the correct SQL query for answering this
+                    question. As a reminder, the question was:{" "}
+                    <span className="font-mono font-semibold dark:text-blue-300">
+                      {question}
+                    </span>
                     <br />
                   </p>
                   <p className="mb-2">
                     The query originally generated by Defog was as follows:
                   </p>
                   <CodeMirror
+                    theme={"dark"}
                     extensions={[codemirrorSql(), EditorView.lineWrapping]}
                     value={sql}
                     basicSetup={{
@@ -166,11 +97,13 @@ const SQLFeedback = ({
                     }}
                     editable={false}
                     minHeight={"200px"}
+                    className="dark:bg-gray-800 rounded-md"
                   />
                   <p className="mb-2">
                     Please provide the correct SQL query below:
                   </p>
                   <CodeMirror
+                    theme={"dark"}
                     extensions={[codemirrorSql(), EditorView.lineWrapping]}
                     value={userProvidedSql}
                     onChange={sqlOnChange}
@@ -179,9 +112,10 @@ const SQLFeedback = ({
                     }}
                     editable={true}
                     minHeight={"200px"}
+                    className="dark:bg-gray-800 rounded-md"
                   />
                   <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs"
+                    className="bg-blue-500 hover:bg-blue-700 text-white dark:bg-blue-600 dark:hover:bg-blue-800 font-bold py-2 px-4 rounded text-xs"
                     onClick={updateGoldenQueries}
                   >
                     Submit SQL
