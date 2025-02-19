412fb0860e55d399d4c54f104a810e20455b6dad
Manas Sharma <sharma.manas271196@gmail.com>
2025-02-19 09:45:45 +0530

    1. remove writer components. 2. remove while (!done) loop in analysis manager. 3. simplify new data merge with simple replaces.

--- lib/components/agent/analysis/Clarify.jsx
+++ lib/components/agent/analysis/Clarify.jsx
@@ -21,8 +21,6 @@ export default function Clarify({
     });
   }
 
-  const [uiDefaults, setUIDefaults] = useState(defaults);
-
   if (!data || !data.clarification_questions || !data.success)
     return (
       <div className="agent-error">
@@ -61,37 +59,6 @@ export default function Clarify({
     }, 100);
   }
 
-  const UIs = {
-    "multi select": (q, i, opts) => {
-      const dropdownOpts = uiDefaults[i] || [];
-
-      return (
-        <MultiSelect
-          options={dropdownOpts}
-          popupClassName="analysis-dropdown"
-          defaultValue={q.response}
-          onChange={(_, allSel) => {
-            return updateAnswer(
-              allSel.map((d) => d.value),
-              i
-            );
-          }}
-          placeholder="Select one or more options"
-        ></MultiSelect>
-      );
-    },
-    "text input": (q, i) => {
-      return (
-        <Input
-          onChange={(ev) => updateAnswer(ev.target.value, i)}
-          defaultValue={q.response}
-          placeholder="Your response. Leave blank if the question above is not relevant"
-          inputClassNames="ring-0 bg-transparent rounded-none border-b border-dotted border-gray-300 focus:border-blue-500 focus:border-solid focus:ring-0 focus:border-b-primary-highlight shadow-none pl-0 w-full"
-        ></Input>
-      );
-    },
-  };
-
   return (
     <div className="p-6 bg-transparent">
       <div
@@ -103,35 +70,31 @@ export default function Clarify({
         }}
       >
         {success &&
-          (clarification_questions.length ? (
-            <>
-              {clarification_questions.map((q, i) => (
+          (clarification_questions.length
+            ? clarification_questions.map((q, i) => (
                 <div key={q.question} className="w-full">
-                  {/* <Writer s={q.question} animate={!stageDone}> */}
                   <div>
                     <p className="q-desc m-0 mb-2 text-primary-text">
                       {q.question}
                     </p>
                     <div className="w-full mb-4 min-w-64">
-                      <p className="q-desc writer-target m-0 mb-2 text-primary-text"></p>
-                      <div className="writer-children">
-                        {UIs["text input"](q, i)}
-                      </div>
+                      <Input
+                        onChange={(ev) => updateAnswer(ev.target.value, i)}
+                        defaultValue={q.response}
+                        placeholder="Your response. Leave blank if the question above is not relevant"
+                        inputClassNames="ring-0 bg-transparent rounded-none border-b border-dotted border-gray-300 focus:border-blue-500 focus:border-solid focus:ring-0 focus:border-b-primary-highlight shadow-none pl-0 w-full"
+                      ></Input>
                     </div>
                   </div>
-                  {/* </Writer> */}
                 </div>
+              ))
+            : !isCurrentStage && (
+                <>
+                  Your question is clear and we do not need additional
+                  refinements. Please click on the Run Analysis button to start
+                  running an analysis that answers this question.
+                </>
               ))}
-            </>
-          ) : (
-            !isCurrentStage && (
-              <>
-                Your question is clear and we do not need additional
-                refinements. Please click on the Run Analysis button to start
-                running an analysis that answers this question.
-              </>
-            )
-          ))}
         {stageDone ? (
           <></>
         ) : (
