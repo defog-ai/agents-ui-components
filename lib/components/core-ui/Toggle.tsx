75611ae644deef2f6d0ffd6fa4d09067ae55f127
Manas Sharma <sharma.manas271196@gmail.com>
2025-03-03 19:25:04 +0530

    update toggle to tsx

--- lib/components/core-ui/Toggle.tsx
+++ lib/components/core-ui/Toggle.tsx
@@ -4,20 +4,73 @@ import { twMerge } from "tailwind-merge";
 
 /**
  * A simple toggle component.
- * @typedef {Object} ToggleProps
- * @property {React.ReactNode} [title=null] - The title of the toggle.
- * @property {React.ReactNode} [onLabel=null] - The label when the toggle is on.
- * @property {React.ReactNode} [offLabel=null] - The label when the toggle is off.
- * @property {boolean} [defaultOn=false] - The default state of the toggle.
- * @property {string} [titleClassNames=""] - Additional classes to be added to the title.
- * @property {string} [onClassNames=""] - Additional classes to be added to the on state.
- * @property {string} [offClassNames=""] - Additional classes to be added to the off state.
- * @property {string} [toggleClassNames=""] - Additional classes to be added to the toggle.
- * @property {string} [rootClassNames=""] - Additional classes to be added to the root div.
- * @property {string} [labelClasses=""] - Additional classes to be added to the label.
- * @property {boolean} [disabled=false] - If true, the toggle will be disabled.
- * @property {function} [onToggle=() => {}] - Function to be called when the toggle is toggled.
  */
+interface ToggleProps {
+  /**
+   * The title of the toggle.
+   */
+  title?: React.ReactNode;
+  /**
+   * The label when the toggle is on.
+   */
+  onLabel?: React.ReactNode;
+  /**
+   * The label when the toggle is off.
+   */
+  offLabel?: React.ReactNode;
+  /**
+   * The default state of the toggle.
+   */
+  defaultOn?: boolean;
+  /**
+   * Size
+   */
+  size?: "small" | "default" | "large";
+  /**
+   * Additional classes to be added to the title.
+   */
+  titleClassNames?: string;
+  /**
+   * Additional classes to be added to the on state.
+   */
+  onClassNames?: string;
+  /**
+   * Additional classes to be added to the off state.
+   */
+  offClassNames?: string;
+  /**
+   * Additional classes to be added to the toggle.
+   */
+  toggleClassNames?: string;
+  /**
+   * Additional classes to be added to the root div.
+   */
+  rootClassNames?: string;
+  /**
+   * Additional classes to be added to the label.
+   */
+  labelClasses?: string;
+  /**
+   * If true, the toggle will be disabled.
+   */
+  disabled?: boolean;
+  /**
+   * Function to be called when the toggle is toggled.
+   */
+  onToggle?: (on: boolean) => void;
+}
+
+const barClassNames = {
+  small: "h-3 w-8",
+  default: "h-5 w-9",
+  large: "h-8 w-11",
+};
+
+const buttonClassNames = {
+  small: "h-2 w-2",
+  default: "h-4 w-4",
+  large: "h-7 w-7",
+};
 
 /**
  * A simple toggle component.
@@ -27,6 +80,7 @@ export function Toggle({
   title = null,
   onLabel = null,
   offLabel = null,
+  size = "default",
   defaultOn = false,
   titleClassNames = "",
   onClassNames = "",
@@ -36,7 +90,7 @@ export function Toggle({
   labelClasses = "",
   disabled = false,
   onToggle = (...args) => {},
-}) {
+}: ToggleProps) {
   const [on, setOn] = useState(defaultOn);
 
   return (
@@ -71,7 +125,8 @@ export function Toggle({
             on
               ? "bg-blue-500 dark:bg-blue-600"
               : "bg-gray-200 dark:bg-gray-700",
-            "relative inline-flex h-6 w-11 flex-shrink-0 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600 focus:ring-offset-2 dark:focus:ring-offset-gray-800",
+            "relative inline-flex items-center flex-shrink-0 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600 focus:ring-offset-2 dark:focus:ring-offset-gray-800 ",
+            barClassNames[size],
             on ? onClassNames : offClassNames,
             !disabled ? "cursor-pointer" : "cursor-not-allowed opacity-50"
           )}
@@ -79,8 +134,9 @@ export function Toggle({
           <span
             aria-hidden="true"
             className={twMerge(
-              on ? "translate-x-5" : "translate-x-0",
-              "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white dark:bg-gray-200 shadow ring-0 transition duration-200 ease-in-out"
+              on ? "right-0" : "right-full translate-x-full",
+              "pointer-events-none absolute transform rounded-full bg-white dark:bg-gray-200 shadow ring-0 transition-all duration-[400ms] ease-in-out",
+              buttonClassNames[size]
             )}
           />
         </Switch>
